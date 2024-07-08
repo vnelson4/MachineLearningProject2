4c6171e09064c9b272fb34881aefefe4ce8becbe
bZwiener <152338408+bZwiener@users.noreply.github.com>
2024-07-08 15:07:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,24 @@
 # Output summaries of each of the models
 model_drop.summary()
 model_l2.summary()
+
+# Compile both models
+model_drop.compile(
+    optimizer="adam",
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+model_l2.compile(
+    optimizer="adam",
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+history_drop = model_drop.fit(
+    train_images, train_labels, epochs=10, validation_data=(test_images, test_labels)
+)
+
+history_l2 = model_drop.fit(
+    train_images, train_labels, epochs=10, validation_data=(test_images, test_labels)
+)

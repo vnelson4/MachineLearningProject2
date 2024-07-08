fbe3a8d48dc3c0f3c71496ad3289aa65945b07b2
lnuss2 <89098726+lnuss2@users.noreply.github.com>
2024-07-08 17:17:57 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+import util
+import model
+import tensorflow as tf 
+from tensorflow.keras import datasets, layers, models
+
 # Code that runs the main loop of training the TensorFlow models
+
+# Instantiate sequential with the name model
 
 # Specify number of epochs for the two models
 epochs_drop = 50
 epochs_l2 = 50
+
+train_images, train_labels, test_images, test_labels, image_shape = util.get_images()
+
+model_drop = model.model_drop_init(image_shape)
+model_l2 = model.model_l2_init(image_shape)
 
 # Output summaries of each of the models
 model_drop.summary()
 model_l2.summary()
 
 # Compile both models
+
 model_drop.compile(
     optimizer="adam",
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
@@ -21,6 +34,7 @@ model_l2.compile(
     metrics=["accuracy"],
 )
 
+
 history_drop = model_drop.fit(
     train_images, train_labels, epochs=epochs_drop, validation_data=(test_images, test_labels)
 )
@@ -28,3 +42,4 @@ history_drop = model_drop.fit(
 history_l2 = model_drop.fit(
     train_images, train_labels, epochs=epochs_l2, validation_data=(test_images, test_labels)
 )
+
--- util.py
+++ util.py
@@ -8,12 +8,13 @@ import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
 
-# Load in CIFAR-100 Dataset
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar100.load_data()
-
-# Normalize pixel values to be between 0 and 1
-# Divides the images by 255 to normalize our pixel intensities to be between 0 and 1 -> Makes it much easier for the NN to train and perform
-train_images, test_images = train_images / 255.0, test_images / 255.0
-
-# Get image shape, needs to be 32x32x3 (32x32 and 3 color panes)
-image_shape = train_images.shape[1:]
+def get_images():
+    # Load in CIFAR-100 Dataset
+    (train_images, train_labels), (test_images, test_labels) = datasets.cifar100.load_data()
+    
+    # Normalize pixel values to be between 0 and 1
+    # Divides the images by 255 to normalize our pixel intensities to be between 0 and 1 -> Makes 
+    # it much easier for the NN to train and perform
+    train_images, test_images = train_images / 255.0, test_images / 255.0
+    image_shape = train_images.shape[1:] # Get image shape, needs to be 32x32x3 (32x32 and 3 color panes)
+    return train_images, train_labels, test_images, test_labels, image_shape

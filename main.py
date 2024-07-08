966c814419d65eb5135662e8e72815879cfaa778
bZwiener <152338408+bZwiener@users.noreply.github.com>
2024-07-08 15:10:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # Code that runs the main loop of training the TensorFlow models
 
+# Specify number of epochs for the two models
+epochs_drop = 50
+epochs_l2 = 50
+
 # Output summaries of each of the models
 model_drop.summary()
 model_l2.summary()
@@ -18,9 +22,9 @@ model_l2.compile(
 )
 
 history_drop = model_drop.fit(
-    train_images, train_labels, epochs=10, validation_data=(test_images, test_labels)
+    train_images, train_labels, epochs=epochs_drop, validation_data=(test_images, test_labels)
 )
 
 history_l2 = model_drop.fit(
-    train_images, train_labels, epochs=10, validation_data=(test_images, test_labels)
+    train_images, train_labels, epochs=epochs_l2, validation_data=(test_images, test_labels)
 )

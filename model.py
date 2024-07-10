05f86609e9e691fc3de3d9b1eb1f573fa95baf52
lnuss2 <89098726+lnuss2@users.noreply.github.com>
2024-07-10 17:05:23 -0500

    Fixed issues with model_drop_init

--- model.py
+++ model.py
@@ -1,31 +1,52 @@
 import util
 import tensorflow as tf 
-from tensorflow.keras import layers, models
+from tensorflow.keras import layers, models, regularizers
 
 #TensorFlow code that defines the network
 
+def model_drop_init(image_shape):
+    model_drop = models.Sequential() # Instantiate sequential with the name model
+    # Add convolutional layers to Model_drop
+    model_drop.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=image_shape)) # 2D convolutional layer with 32 filters, (3x3 convolutions), (diff from image sizes)
+    model_drop.add(layers.MaxPooling2D((2, 2))) # Drags a 2x2 kernal across its input, chooses the max for each one, go to output
+    model_drop.add(layers.Conv2D(64, (3, 3), activation="relu")) # Convolutional layer also 3x3, 62 filters, 
+    model_drop.add(layers.MaxPooling2D((2, 2))) # 2x2 pooling
+    model_drop.add(layers.Conv2D(64, (3, 3), activation="relu"))
+    model_drop.add(layers.Flatten()) # Flattens 2 dimesnonal input into a 1 dimensional
+    model_drop.add(layers.Dense(64, activation="relu")) # Regular dense layer of 64 nodes, relu activation
+    model_drop.add(layers.Dense(100)) # Output dense layer where # of nodes = # of classes 
+    return model_drop
+
+def model_l2_init(image_shape):
+    model_l2 = models.Sequential() # Instantiate sequential with the name model
+    # Add convolutional layers to Model_l2
+    model_l2.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=image_shape)) # 2D convolutional layer with 32 filters, (3x3 convolutions), (diff from image sizes)
+    model_l2.add(layers.MaxPooling2D((2, 2))) # Drags a 2x2 kernal across its input, chooses the max for each one, go to output
+    model_l2.add(layers.Conv2D(64, (3, 3), activation="relu")) # Convolutional layer also 3x3, 62 filters, 
+    model_l2.add(layers.MaxPooling2D((2, 2))) # 2x2 pooling
+    model_l2.add(layers.Conv2D(64, (3, 3), activation="relu")) 
+    model_l2.add(layers.Flatten()) # Flattens 2 dimesnonal input into a 1 dimensional
+    model_l2.add(layers.Dense(64, activation="relu")) # Regular dense layer of 64 nodes, relu activation
+    model_l2.add(layers.Dense(100)) # Output dense layer where # of nodes = # of classes 
+    return model_l2
+
+"""
 # Create cnn model with 4 layers of convolution and pooling and drop regularizer
 def model_drop_init(image_shape):
     model_drop = models.Sequential() # Instantiate sequential with the name model
     # Add convolutional layers to Model_drop
     model_drop.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=image_shape)) # 2D convolutional layer with 32 filters, (3x3 convolutions), (diff from image sizes)
     model_drop.add(layers.MaxPooling2D((2, 2))) # Drags a 2x2 kernal across its input, chooses the max for each one, go to output
-    model.add(layers.Dropout(0.5))
+    model_drop.add(layers.Dropout(0.5))
     model_drop.add(layers.Conv2D(64, (3, 3), activation="relu")) # Convolutional layer also 3x3, 62 filters, 
     model_drop.add(layers.MaxPooling2D((2, 2))) # 2x2 pooling
-    model.add(layers.Dropout(0.5))
+    model_drop.add(layers.Dropout(0.5))
     model_drop.add(layers.Conv2D(64, (3, 3), activation="relu"))  
-    model_drop.add(layers.MaxPooling2D((2, 2))) # 2x2 pooling
-    model.add(layers.Dropout(0.5))
-    model_drop.add(layers.Conv2D(64, (3, 3), activation="relu"))  
-    model_drop.add(layers.MaxPooling2D((2, 2))) # 2x2 pooling
-    model.add(layers.Dropout(0.5))
-    model_drop.add(layers.Conv2D(64, (3, 3), activation="relu"))
     model_drop.add(layers.Flatten()) # Flattens 2 dimesnonal input into a 1 dimensional
     model_drop.add(layers.Dense(64, activation="relu")) # Regular dense layer of 64 nodes, relu activation
-    model.add(layers.Dropout(0.5))
+    model_drop.add(layers.Dropout(0.5))
     model_drop.add(layers.Dense(100)) # Output dense layer where # of nodes = # of classes 
-    model_drop.add(layers.Softmax())
+    #model_drop.add(layers.Softmax())
     return model_drop
 
 # Create cnn model with 2 layers of convolution and pooling and l2 regularizer
@@ -40,5 +61,7 @@ def model_l2_init(image_shape):
     model_l2.add(layers.Flatten()) # Flattens 2 dimesnonal input into a 1 dimensional
     model_l2.add(layers.Dense(64, activation="relu", kernel_regularizer=regularizers.l2(0.01))) # Regular dense layer of 64 nodes, relu activation
     model_l2.add(layers.Dense(100)) # Output dense layer where # of nodes = # of classes 
-    model_drop.add(layers.Softmax())
+    #model_l2.add(layers.Softmax())
     return model_l2
+"""
+    
